8a35df51b19c5d23e1566eccb06afa98ec6c50ca
cmotuzas <cmotuzas@uwo.ca>
2024-12-04 16:17:54 -0500

    Create MotuzasCharlotte_Project4.py
    initial project description notes

---
+++ MotuzasCharlotte_Project4.py
@@ -0,0 +1,30 @@
+# Project 4 
+
+import numpy as np 
+import scipy as scp 
+import matplotlib.pyplot as plt 
+
+# develop a python function that solves the one dimensional, time-dependent Schroedinger Eqn
+# section 9.2 of the textbook 
+# equations 9.32 and 9.40 in the text 
+# main function can call additional functions that you write 
+
+# sch_eqn(nspace, ntime, tau, method='ftcs', length=200, potential = [], wparam = [10, 0, 0.5])
+'''
+method: string, either ftcs or crank
+length: float, size of spatial grid. Default to 200 (grid extends from -100 to +100)
+potential: 1-D array giving the spatial index values at which the potential V(x) should be set to 1. Default to empty. For example, [25, 50] will set V[25] = V[50] = 1.
+wparam: list of parameters for initial condition [sigma0, x0, k0]. Default [10, 0, 0.5].'''
+
+# see eqn 9.42 in the text as well 
+# periodic boundary conditions, see lab 11 
+# return 2D array, two 1D arrays giving x and t, and 1D array that gives total prob for each timestep (conserved)
+# additional sch_plot function that uses output to visualize results 
+# plot of psi at a specific time t
+# plot of prob, plot of the particle prob density at a specific time 
+# numpy.conjugate to do complex conjugation 
+
+# use comments to describe the origins of the code, if taken from NM4P or prior labs 
+
+# report, function documentation, report on how you tested your functions 
+# write so that it may be used by an end user 
