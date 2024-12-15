e32c29512f0605771ce69074c4c3a87d3abdca67
cmotuzas <cmotuzas@uwo.ca>
2024-12-15 13:00:28 -0500

    Update MotuzasCharlotte_Project4.py
    adjusting periodic boundary conditions and plotting

--- MotuzasCharlotte_Project4.py
+++ MotuzasCharlotte_Project4.py
@@ -55,12 +55,17 @@ def sch_eqn(nspace, ntime, tau, method='ftcs', length=200, potential = [], wpara
     b = 1
     a = 1
     H = make_tridiagonal(nspace, b, d, a)
-    H[0, -1] = b
-    H[-1, 0] = a
+    H[0,0] = d
+    H[0, -1] = a
+    H[-1, 0] = b
+    H[-1,-1] = d
+    H[-1,-2] = a 
+    H[0,1] = b
+
     H = -coeff*H + V*np.identity(nspace)
 
     if method == 'ftcs': 
-        A = (np.identity(nspace) - (1j*tau/hbar)*H)
+        A = (np.identity(nspace) - (1j*tau/2*hbar)*H)
 
         # Check spectral radius
         sr = spectral_radius(A)
@@ -71,7 +76,7 @@ def sch_eqn(nspace, ntime, tau, method='ftcs', length=200, potential = [], wpara
             print("Seems good!!")
 
     elif method == 'crank': 
-        A = np.linalg.inv((np.identity(nspace) + (1j*tau/hbar)*H)).dot(np.identity(nspace) - (1j*tau/hbar)*H)
+        A = np.linalg.inv((np.identity(nspace) + (1j*tau/2*hbar)*H)).dot(np.identity(nspace) - (1j*tau/2*hbar)*H)
 
     else: 
         print("Please enter either 'ftcs' or 'crank' as the method input")    
@@ -167,13 +172,12 @@ def sch_eqnNM4P(nspace, ntime, tau, method='ftcs', length=200, potential = [], w
     return psi, x, t, prob
 
 
-
 def sch_plot(psi,x,t,prob,ntime,plot,save): 
     N = int(np.round(ntime)/8)
     if plot == 'psi':
         fig = plt.figure()
         for i in range(8): 
-            plt.plot(x,np.real(psi[:,N*i]),label='step {}'.format(N*i))
+            plt.plot(x,np.real(psi[:,N*i]),label='t = {}'.format(t[N*i]))
         plt.title('Schrodinger Wave Equation Results')
         plt.xlabel('Position (x)')
         plt.ylabel('$\\psi$ (x, t)')
@@ -195,6 +199,27 @@ def sch_plot(psi,x,t,prob,ntime,plot,save):
     
     return 
 
-ntime = 5000
-psi, x, t, prob = sch_eqnNM4P(100, ntime, 0.03, method='crank', length=200, potential = [], wparam = [10, 0, 0.5])
-sch_plot(psi,x,t,prob,ntime,'psi',1)
+ntime = 2000
+psi, x, t, prob = sch_eqn(200, ntime, 0.03, method='crank', length=200, potential = [], wparam = [10, 0, 0.5])
+#sch_plot(psi,x,t,prob,ntime,'psi',1)
+
+# from lab 11 
+
+plotskip = round(0.05*ntime)
+fig, ax = plt.subplots()
+# space out the plots vertically to make the visualization clearer
+yoffset = psi[:,0].max() - psi[:,0].min()
+for i in np.arange(len(t)-1,0,-plotskip): 
+    ax.plot(x, psi[:,i]+yoffset*i/plotskip,label = 't ={:.3f}'.format(t[i]))
+ax.set_xlabel('X position')
+ax.set_ylabel('$\\psi$(x,t) [offset]')
+
+# Shrink current axis by 20%
+box = ax.get_position()
+ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+
+# Put a legend to the right of the current axis
+ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+
+ax.set_title('Wave Propagation with Offsets')
+plt.show()
